31c48eb76f3c9c7207ff3598e81a3140ca15ca29
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2020-04-30 14:44:02 +0630

    newly add

--- home/views.py
+++ home/views.py
@@ -5,8 +5,9 @@ from agents.models import Agent
 from django.db.models import Count
 # Create your views here.
 def home(request):
-	category_list = Category.objects.annotate(property_count=Count('property')).values('category_name', 'property_count', 'image')
-	print(category_list)
+	category_list = Category.objects.all()
+	# category_list = Category.objects.annotate(property_count=Count('property')).values('category_name', 'property_count', 'image')
+	# print(category_list)
 	property_lists = Property.objects.all()
 	home_list = HomeModel.objects.all()
 	agent_list = Agent.objects.all()
